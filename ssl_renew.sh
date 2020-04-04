3b755464aa7b525dfaa0319d169bbdb12a2812ec
luozhouyang <zhouyang.luo@gmail.com>
2020-04-04 22:12:57 +0800

    Remove --dry-run argument

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,5 +4,5 @@ COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
 cd /root/GitHub/luozhouyang/wordpress/
-$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
